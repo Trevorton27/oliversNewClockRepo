746dec173e7cc1e814a49a4daf51b8130d273266
Trevor Mearns <spiral272@gmail.com>
2021-09-25 11:15:40 +0900

    first commit

---
+++ clock.js
@@ -0,0 +1,114 @@
+/*
+setTimeout(() => {
+    console.log('time!');
+}, 1000);
+
+let i = 0;
+
+function increment() {
+    i++;
+    console.log(i);
+}
+
+let timer = setTimeout(function myTimer() {
+    increment();
+    timer = setTimeout(myTimer, 1000);
+
+}, 1000);
+
+var today = new Date();
+console.log(today);
+
+
+// console.dir(document);
+
+console.log(document.body);
+
+// get element by id 
+
+// console.log(document.getElementById("time-display"));
+
+var displayTime = document.getElementById("time-display");
+console.log(displayTime);
+displayTime.textContent = (1); {
+
+
+*/
+
+function simpleClock() {
+    const date = new Date();
+    const months = ["January", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    const weekDays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday",];
+
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDay();
+
+
+    // even by selecting  get days VALUE FROM MY VARIABLE LET DAY its still showing up as saturday"
+    // this display the right day 
+
+    // IS MY MONTH ALSO RIGHT OR AM I JUST SEPTEMBER RANDOMLY LUCKY ?
+
+    document.getElementById("time-display").textContent = months[date.getMonth()] + ' ' + date.getFullYear() + " " + weekDays[date.getDay()] + " " + [date.getDate()] + ("th");
+
+
+
+
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+
+    if (hours <= 12) {
+        seconds = seconds + "AM";
+    } else {
+        seconds = seconds + "Pm";
+    }
+
+    if (hours === 0) {
+        hours = 12;
+    }
+
+
+
+    document.getElementById("date-display").textContent = ` ${hours} : ${minutes} : ${seconds}`;
+
+
+
+}
+
+setInterval(simpleClock, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
